eff7700ec507a51af8ccdd243aca04d06a3bebd5
triciahughes <hughes.a.tricia@gmail.com>
2023-02-11 01:26:20 -0800

    nav bar update

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <header>
+      <div className="navbar">
+        <a>Games</a>
+        <a>Developers</a>
+        <a>My Saved Games</a>
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
