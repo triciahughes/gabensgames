ca920ccdc533a3901f0676e1c05a2bcd567c3e25
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-02-13 16:20:43 -0700

    list of games in Games

--- src/Games.js
+++ src/Games.js
@@ -1,20 +1,23 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const API_KEY = 'a77f9483f3f14f5292692794e7fba92a';
 
 function Games() {
+    const [ games, setGames ] = useState([]);
+
+    const gameList = games.map(gameObj => {
+        return <li key={gameObj.id}>{gameObj.name}</li>;
+    });
 
     useEffect(() => {
-        fetch(`https://api.rawg.io/api/platforms?key=${API_KEY}`, {
-            headers: {
-
-            }
-        })
+        fetch(`https://api.rawg.io/api/games?key=${API_KEY}`)
           .then(r => r.json())
-          .then(console.log);
+          .then(data => setGames(data.results));
     }, []);
 
-    return <h1>GAMES</h1>;
+    return (
+        <ul>{gameList}</ul>
+    );
 }
 
 export default Games;
