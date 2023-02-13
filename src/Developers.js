828e4f32a8c2ed8133b05cd1a9dafa6791b07991
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-02-13 16:26:14 -0700

    game devs list

--- src/Developers.js
+++ src/Developers.js
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
 import DeveloperItem from "./DeveloperItem";
 
+const API_KEY = 'a77f9483f3f14f5292692794e7fba92a';
+
 function Developers() {
-    return <h1>DEVELOPERS</h1>;
+    const [ devs, setDevs ] = useState([]);
+
+    const devList = devs.map(gameObj => {
+        return <li key={gameObj.id}>{gameObj.name}</li>;
+    });
+
+    useEffect(() => {
+        fetch(`https://api.rawg.io/api/developers?key=${API_KEY}`)
+          .then(r => r.json())
+          .then(data => setDevs(data.results));
+    }, []);
+
+    return (
+        <ul>{devList}</ul>
+    );
 }
 
 export default Developers;
