5d86c0e4fd8190a9b7b45759a0b893f959252838
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-02-13 16:13:26 -0700

    connection to API in Games

--- src/Games.js
+++ src/Games.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
+const API_KEY = 'a77f9483f3f14f5292692794e7fba92a';
+
 function Games() {
+
+    useEffect(() => {
+        fetch(`https://api.rawg.io/api/platforms?key=${API_KEY}`, {
+            headers: {
+
+            }
+        })
+          .then(r => r.json())
+          .then(console.log);
+    }, []);
+
     return <h1>GAMES</h1>;
 }
 
