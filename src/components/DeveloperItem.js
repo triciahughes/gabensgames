cd528e4adacf2bc9ec82cf63c57b4dcc3d31243e
triciahughes <hughes.a.tricia@gmail.com>
2023-02-14 10:23:43 -0800

    search working with dev

--- src/components/DeveloperItem.js
+++ src/components/DeveloperItem.js
@@ -1,22 +1,22 @@
 function DeveloperItem({ dev }) {
-    console.log(dev);
-    const { name, games, games_count } = dev;
+  console.log(dev);
+  const { name, games, games_count } = dev;
 
-    const gamesList = games.map(gameObj => {
-        return <li key={gameObj.id}>{gameObj.name}</li>;
-    });
+  const gamesList = games.map((gameObj) => {
+    return <li key={gameObj.id}>{gameObj.name}</li>;
+  });
 
-    return (
-      <li className="cards-item">
-        <div className="card" >  
-            <h1>{name}</h1>
-            <ul>
-              {gamesList}
-            </ul>
-            <p><i>{games_count} games</i></p>
-        </div>
-      </li>
-    );
+  return (
+    <li className="cards-item">
+      <div className="card">
+        <h1>{name}</h1>
+        <ul>{gamesList}</ul>
+        <p>
+          <i>{games_count} games</i>
+        </p>
+      </div>
+    </li>
+  );
 }
 
 export default DeveloperItem;
