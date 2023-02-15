f8ba3d37987d4ab772406570f5e602fc315a62ed
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-02-15 10:00:11 -0700

    fixed page refresh routing

--- src/components/DeveloperItem.js
+++ src/components/DeveloperItem.js
@@ -1,20 +1,14 @@
 function DeveloperItem({ dev }) {
 const { name, games, games_count, image_background } = dev;
 
-//   const gamesList = games.map((gameObj) => {
-//     return <li key={gameObj.id}>{gameObj.name}</li>;
-//   });
-
 const gameTitles = games.map(gameObj => gameObj.name);
 const gamesString = Array.from(gameTitles).join(' - ');
-
 
   return (
     <li className="cards-item">
       <div className="card dev-card" style={{'backgroundImage': `url(${image_background})`}}>
         <div className="card-content dev-card-content">
           <h1 className="card-title">{name}</h1>
-          {/* <ul>{gamesList}</ul> */}
           <p>
             {gamesString}
             <br />
