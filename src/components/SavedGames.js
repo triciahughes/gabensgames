5a1bfc5e6d76166956e94c801504e934c8935d75
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-02-16 13:57:02 -0700

    message for when no games saved

--- src/components/SavedGames.js
+++ src/components/SavedGames.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Multiselect from "multiselect-react-dropdown";
 
 function SavedGames({ games, handleAddGame, handleRemove }) {
+
   const initialFormValues = {
     name: "",
     genres: [],
@@ -66,6 +67,97 @@ function SavedGames({ games, handleAddGame, handleRemove }) {
 
   function handleClickToggle() {
     setShowForm(showForm => !showForm);
+  }
+
+  if (games.length == 0) {
+    return (
+      <>
+        <h1>Click "Save Game" To Save Games Here!</h1>
+        <button 
+          className="toggle-form" 
+          onClick={handleClickToggle} 
+        >
+          {showForm ? 'Close Form' : 'Add New Game'}
+        </button>
+        {showForm ? (<div className="new-form-container">
+        <form 
+          onSubmit={handleSubmit} 
+          autoComplete="off" 
+          className="new-form"
+        >
+          <label htmlFor="title">Game Title: </label>
+          <input
+            type="text"
+            id={formData.name}
+            name="name"
+            value={formData.name}
+            onChange={handleInput}
+          />
+          <br />
+          <label htmlFor="img">Image Url: </label>
+          <input
+            type="text"
+            id={formData.url}
+            name="background_image"
+            value={formData.background_image}
+            onChange={handleInput}
+          />
+          <br />
+          <label htmlFor="metacritic">Metacritic: </label>
+          <input
+            type="number"
+            min="0"
+            max="100"
+            id={formData.metacritic}
+            name="metacritic"
+            value={formData.metacritic}
+            onChange={handleInput}
+          />
+          <label htmlFor="esrb_rating">ESRB: </label>
+          <select
+            id={formData.esrb_rating}
+            name="esrb_rating"
+            value={formData.esrb_rating}
+            onChange={handleInput}
+          >
+            <option value="N/A">Choose</option>
+            <option>Early Childhood</option>
+            <option>Everyone</option>
+            <option>Everyone 10+</option>
+            <option>Teen</option>
+            <option>Mature</option>
+            <option>Adults Only</option>
+            <option>Rating Pending</option>
+          </select>
+          
+          <div className="multiselect-container">
+            <label htmlFor="genres">Genres: </label>
+            <Multiselect
+              className="multiselect"
+              selectedValues={formData.genres}
+              isObject={false}
+              onKeyPressFn={function noRefCheck() {}}
+              onSelect={handleOnSelect}
+              options={[
+                "Sandbox",
+                "Real-time strategy (RTS)",
+                "Shooter",
+                "MOBA",
+                "Role-playing (RPG, ARPG, and More)",
+                "Simulation and sports",
+                "Puzzlers and party games",
+                "Action-adventure",
+                "Survival and horror",
+                "Platformer",
+              ]}
+            />
+          </div>
+          <button type="submit">Add Game</button>
+        </form>
+      </div>
+      ) : null }
+      </>
+    );
   }
 
   return (
