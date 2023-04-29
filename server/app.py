dab71c108bf208d640ee027eb91e433c3539009d
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-04-29 16:02:12 -0700

    backend working

---
+++ server/app.py
@@ -0,0 +1,70 @@
+
+# Remote library imports
+from flask import Flask, request, session, make_response, jsonify, abort, render_template
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from werkzeug.exceptions import NotFound, Unauthorized
+from config import app, db, api
+from flask_cors import CORS
+
+CORS(app)
+
+@app.route('/')
+@app.route('/<int:id>')
+def index(id=0):
+    return render_template("index.html")
+
+# Local imports
+from config import app, db, api
+
+api = Flask(__name__)
+
+class Signin(Resource):
+    def post(self):
+
+        data = request.get_json()
+
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter(User.username == username).first()
+
+            
+        if user.authenticate(data['password']):
+
+            session['user_id'] = user.id
+
+            response = make_response(
+                user.to_dict(),
+                200
+            )
+            return response
+        return {'error' : "Invalid Username or Password"}, 401
+
+class Signup(Resource):
+
+    def post(self):
+
+        data = request.get_json()
+
+        new_user = User(
+
+            username=data['username']
+        )
+
+        new_user.password_hash = data['password']
+
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['user_id'] = new_user.id
+
+        response = make_response(
+            new_user.to_dict(),
+            201
+        )
+        return response
+
+api.add_resource(Signin, '/signin')
+api.add_resource(Signup, '/signup')
---
+++ server/config.py
@@ -0,0 +1,36 @@
+# Remote library imports
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Instantiate app, set attributes
+app = Flask(__name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+
+
+# Define metadata, instantiate db
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate(app, db)
+db.init_app(app)
+
+bcrypt = Bcrypt(app)
+
+# Instantiate REST API
+api = Api(app)
